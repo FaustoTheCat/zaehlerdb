cbeebfed69a21380a4a9e117e7fb2ceba01bcea0
FaustoTheCat <46087000+FaustoTheCat@users.noreply.github.com>
2021-08-31 14:04:22 +0200

    Testseite erzeugt

---
+++ testpage.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<style type="text/css">
+<!--
+input.error {
+    background-color: red;
+}
+-->
+</style>
+</head>
+<?php
+include "dbtools.php";
+
+?>
+<body>
+Hier soll eine Optionsbox erscheinen, also bald<BR>
+<?php
+ausgabe_uhrzeit();
+?>
+</body>
+</html>
